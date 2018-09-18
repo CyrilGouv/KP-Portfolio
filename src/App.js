2bbc3f5b52bd8f4bf67e3875ef96d4690176cbf8
Cyril Gouverneur <cyril@wifibridge.home>
2018-09-18 17:40:18 +0200

    Make slide in & slide out navigation

--- src/App.js
+++ src/App.js
@@ -11,16 +11,33 @@ import Footer from './Components/Footer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navIsOpen: false
+    }
+
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+
+  openMenu() {
+    this.setState({ navIsOpen: !this.state.navIsOpen });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <Nav />
-        <Hero />
-        <Welcome />
-        <About />
-        <Works />
-        <Contact />
-        <Footer />
+        <Nav show={this.state.navIsOpen ? true : false} />
+        <div className="wrapper" style={{ 'transform': `translateX(${this.state.navIsOpen ? 80 : 0}%)` }}>
+          <Hero navIsOpen={ this.openMenu } isOpen={this.state.navIsOpen ? 'block' : 'none'} isClose={this.state.navIsOpen ? 'none' : 'block'} />
+          <Welcome />
+          <About />
+          <Works />
+          <Contact />
+          <Footer />
+        </div>
       </div>
     );
   }
